9c116ef21a5fe3c2a53f55d657e3e924f19d4125
alwayz0121 <alwayz0121@gmail.com>
2024-05-18 14:06:49 +0900

    design: Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import Home from "./Routes/Home";
+import Tv from "./Routes/Tv";
+import Search from "./Routes/Search";
+import Header from "./Components/Header";
 
 function App() {
   return (
-    <>
-      <div>Hello</div>
-    </>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/tv" element={<Tv />}></Route>
+        <Route path="/search" element={<Search />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
